f8b43bfe6949bcecdabbae4d81770d7fdd8a634d
Dalveer Singh <dalveersidhu97@gmail.com>
2023-12-27 23:10:07 -0700

    removed experimental stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,8 @@
-import { Categories, Categories2 } from '@/components/categories';
-import { Suspense } from 'react';
-
 export default async function Home () {
   
   return (
     <main className='flex flex-col gap-4'>
-      <Suspense fallback={<div>...loading categories</div>}>
-        <Categories></Categories>
-      </Suspense>
-      <Suspense fallback={<div>...loading categories 2</div>}>
-        <Categories2></Categories2>
-      </Suspense>
+      Front-End
     </main>
   )
 }
